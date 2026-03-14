ee0c8b53f462aa362fa3175be36f01277b8c9895
Bones <terraw1999@proton.me>
2026-03-14 12:05:42 -0600

    Add: Reset loop to clear IO_BANK0

--- blink.c
+++ blink.c
@@ -12,9 +12,30 @@ Alternates between blinking the onboard LED at GPIO 25 and the offboard LED at G
 #define SIO_BASE	(*(volatile uint32_t *)0xd0000000)	// SIO base
 #define BIG_NUM		(*(volatile uint32_t *)0x00780000)	// big number for delay
 
-int main() {
+// Check if reset is cleared - if bit 5 of RST_BASE == 1, reset is cleared
+void rst() {
 
 	// Release IO_BANK0 from reset controller
+	RST_CLR |= 1 << 5; // Clear all bits and set bit 5 to 1
+
+	while (1) {
+		
+		// Check if bit 5 is not 1
+		if (!(RST_BASE & (1 << 5))) {
+			// Do nothing (Wait)
+		}
+	}
+}
+
+// Set SIO control over GPIO pins 0 and 25
+void ctrl() {
+
 	
+
+}
+
+int main() {
+
+	rst();
 
 }
