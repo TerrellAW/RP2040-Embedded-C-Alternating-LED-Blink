613d3c7584de8795d2c75f709382183f993ab6af
Bones <terraw1999@proton.me>
2026-03-14 14:56:03 -0600

    Add: Blinking loop, delay and pin control

--- blink.c
+++ blink.c
@@ -5,12 +5,16 @@ Alternates between blinking the onboard LED at GPIO 25 and the offboard LED at G
 */
 
 // Define macros
-#define RST_CLR		(*(volatile uint32_t *)0x4000f000)	// atomic register to clear reset controller
-#define RST_BASE	(*(volatile uint32_t *)0x4000c000)	// reset controller base
+#define RST_CLR		(*(volatile uint32_t *)0x4000f000)	// reset clear value
+#define RST_BASE	(*(volatile uint32_t *)0x4000c000)	// reset base value
 #define CTRL_25		(*(volatile uint32_t *)0x400140cc)	// GPIO25 controller
 #define CTRL_0 		(*(volatile uint32_t *)0x40014004)	// GPIO0 controller
-#define SIO_BASE	(*(volatile uint32_t *)0xd0000000)	// SIO base
+#define SIO_BASE	((volatile uint32_t *)0xd0000000)	// SIO base register
 #define BIG_NUM		(*(volatile uint32_t *)0x00780000)	// big number for delay
+
+// Define constants
+const uint32_t GPIO0_MASK = 1;
+const uint32_t GPIO25_MASK = 1 << 25;
 
 // Check if reset is cleared - if bit 5 of RST_BASE == 1, reset is cleared
 void rst() {
@@ -30,12 +34,56 @@ void rst() {
 // Set SIO control over GPIO pins 0 and 25
 void ctrl() {
 
-	
+	// Set SIO control over GPIO 0 (EXTERN)
+	CTRL_0 = 5;
+	// Enable GPIO 0 (EXTERN)
+	SIO_BASE[36] = GPIO0_MASK;
 
+	// Set SIO control over GPIO 25 (LED)
+	CTRL_25 = 5;
+	// Enable GPIO 25 (LED)
+	SIO_BASE[36] = GPIO25_MASK;
+
+}
+
+// Delay countdown
+void delay() {
+	uint32_t count = BIG_NUM;
+	while (count > 0) {
+		count--;
+	}
+}
+
+// Loop to blink LEDs
+void led_loop() {
+
+	while (1) {
+
+		// Turn on GPIO25
+		SIO_BASE[20] = GPIO25_MASK;
+		delay();
+
+		// Turn off GPIO25
+		SIO_BASE[24] = GPIO25_MASK;
+		delay();
+
+		// Turn on GPIO0
+		SIO_BASE[20] = GPIO0_MASK;
+		delay();
+
+		// Turn off GPIO0
+		SIO_BASE[24] = GPIO0_MASK;
+		delay();
+
+	}
 }
 
 int main() {
 
 	rst();
+
+	ctrl();
+
+	led_loop();
 
 }
