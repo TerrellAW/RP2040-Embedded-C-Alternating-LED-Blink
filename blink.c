e9a60a406f970d5c36fbcf4fea401ad275eda3a7
Bones <terraw1999@proton.me>
2026-07-20 17:50:30 -0600

    Fix: Faulty pointer arithmetic

--- blink.c
+++ blink.c
@@ -7,40 +7,40 @@ Alternates between blinking the onboard LED at GPIO 25 and the offboard LED at G
 #include <stdint.h>
 
 // Define macros
-#define RST_CLR		(*(volatile uint32_t *)0x4000f000)	// reset clear value
-#define RST_BASE	(*(volatile uint32_t *)0x4000c000)	// reset base value
-#define CTRL_25		(*(volatile uint32_t *)0x400140cc)	// GPIO25 controller
-#define CTRL_0 		(*(volatile uint32_t *)0x40014004)	// GPIO0 controller
+#define RST_CLR		((volatile uint32_t *)0x4000f000)	// reset clear value
+#define RST_BASE	((volatile uint32_t *)0x4000c000)	// reset base value
+#define CTRL_25		((volatile uint32_t *)0x400140cc)	// GPIO25 controller
+#define CTRL_0 		((volatile uint32_t *)0x40014004)	// GPIO0 controller
 #define SIO_BASE	((volatile uint32_t *)0xd0000000)	// SIO base register
-#define BIG_NUM		(*(volatile uint32_t *)0x00780000)	// big number for delay
+#define BIG_NUM		((volatile uint32_t)0x00780000)	// big number for delay
 
-// Define constants
-const uint32_t GPIO0_MASK = 1;
-const uint32_t GPIO25_MASK = 1 << 25;
+// Bit masks
+#define GPIO25_MASK (1UL << 25)
+#define GPIO0_MASK (1UL << 0)
 
 // Check if reset is cleared - if bit 5 of RST_BASE == 1, reset is cleared
 void rst() {
 
 	// Release IO_BANK0 from reset controller
-	RST_CLR = 1 << 5; // Clear all bits and set bit 5 to 1
+	*RST_CLR = 1 << 5; // Clear all bits and set bit 5 to 1
 
-	/*while (!(RST_BASE & (1 << 5))) {
+	while ((RST_BASE[2] & (1 << 5)) == 0) {
 		// Do nothing (Wait)
-	}*/
+	}
 }
 
 // Set SIO control over GPIO pins 0 and 25
 void ctrl() {
 
 	// Set SIO control over GPIO 0 (EXTERN)
-	CTRL_0 = 5;
+	*CTRL_0 = 5;
 	// Enable GPIO 0 (EXTERN)
-	SIO_BASE[36] = GPIO0_MASK;
+	SIO_BASE[9] = GPIO0_MASK;
 
 	// Set SIO control over GPIO 25 (LED)
-	CTRL_25 = 5;
+	*CTRL_25 = 5;
 	// Enable GPIO 25 (LED)
-	SIO_BASE[36] = GPIO25_MASK;
+	SIO_BASE[9] = GPIO25_MASK;
 
 }
 
@@ -58,19 +58,19 @@ void led_loop() {
 	while (1) {
 
 		// Turn on GPIO25
-		SIO_BASE[20] = GPIO25_MASK;
+		SIO_BASE[5] = GPIO25_MASK;
 		delay();
 
 		// Turn off GPIO25
-		SIO_BASE[24] = GPIO25_MASK;
+		SIO_BASE[6] = GPIO25_MASK;
 		delay();
 
 		// Turn on GPIO0
-		SIO_BASE[20] = GPIO0_MASK;
+		SIO_BASE[5] = GPIO0_MASK;
 		delay();
 
 		// Turn off GPIO0
-		SIO_BASE[24] = GPIO0_MASK;
+		SIO_BASE[6] = GPIO0_MASK;
 		delay();
 
 	}
